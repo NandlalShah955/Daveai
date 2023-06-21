55754f8ccdbba6ba51774f36096ec6c7889a7d9a
Nandlal <shahnandlal49@gmail.com>
2023-06-22 00:34:41 +0530

    all cards are added

--- stuff/index.js
+++ stuff/index.js
@@ -1,67 +1,75 @@
+let formdata = JSON.parse(localStorage.getItem("data")) || [];
 
-let formdata=JSON.parse(localStorage.getItem("data")) ||[];
+document.getElementById("employeedata").addEventListener("submit", table);
+function table(event) {
+  event.preventDefault();
+  window.location.reload();
+  let form = document.getElementById("employeedata");
 
-document.getElementById("employeedata").addEventListener("submit",table)
-function table(event){
-event.preventDefault();
-window.location.reload();
-let form=document.getElementById("employeedata");
+  let firstname = form.firstname.value;
+  let lastname = form.lastname.value;
+  let birthdate = form.birthdate.value;
+  let email = form.email.value;
+  let mobile = form.mbl.value;
+  let city = form.city.value;
+  let pincode = form.pincode.value;
 
-let firstname=form.firstname.value;
-let lastname=form.lastname.value;
-let birthdate=form.birthdate.value;
-let email=form.email.value;
-let mobile=form.mbl.value;
-let city=form.city.value;
-let pincode=form.pincode.value;
+  let datafromconstruct = new construct(
+    firstname,
+    lastname,
+    birthdate,
+    email,
+    mobile,
+    city,
+    pincode
+  );
 
-let datafromconstruct=new construct(firstname,lastname,birthdate,email,mobile,city,pincode)
-
-formdata.push(datafromconstruct);
-localStorage.setItem("data",JSON.stringify(formdata));
+  formdata.push(datafromconstruct);
+  localStorage.setItem("data", JSON.stringify(formdata));
 }
 
 // Creating a constructor function;
 
-function construct(f,l,b,e,m,c,p){
-    this.firstname=f;
-    this.lastname=l;
-    this.birthdate=b;
-    this.email=e;
-    this.mobile=m;
-    this.city=c;
-    this.pincode=p;
+function construct(f, l, b, e, m, c, p) {
+  this.firstname = f;
+  this.lastname = l;
+  this.birthdate = b;
+  this.email = e;
+  this.mobile = m;
+  this.city = c;
+  this.pincode = p;
 }
 
 // Creating a function for appending data of the form in table;
 
- const append=(formdata)=>{
+const append = (formdata) => {
+  let all_products = document.getElementById("all_products");
+  all_products.innerHTML = null;
+  formdata.map(function (elem) {
+    let box = document.createElement("div");
+    box.setAttribute("class", "box");
 
-  formdata.map(function(elem){
-  
-    var tr=document.createElement("tr");
-    
-    var td1=document.createElement("td");
-    td1.innerText=elem.firstname
-    var td2=document.createElement("td");
-    td2.innerText=elem.lastname
-    var td3=document.createElement("td");
-    td3.innerText=elem.birthdate
-    var td4=document.createElement("td");
-    td4.innerText=elem.email
-    var td5=document.createElement("td");
-    td5.innerText=elem.mobile
-    var td6=document.createElement("td");
-    td6.innerText=elem.city
-    var td7=document.createElement("td");
-    td7.innerText=elem.pincode
+    let image1 = document.createElement("img");
+    image1.src =
+      "https://th.bing.com/th/id/OIP.eXWcaYbEtO2uuexHM8sAwwHaHa?w=174&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7";
 
-    
-tr.append(td1,td2,td3,td4,td5,td6,td7);
-document.querySelector("tbody").append(tr);
+    let firstname = document.createElement("h2");
+    firstname.innerText = `${elem.firstname} ${elem.lastname}`;
 
+    let dateofbirth = document.createElement("p");
+    dateofbirth.innerText = `Birthdate:${elem.birthdate}`;
+    let email = document.createElement("p");
+    email.innerText = `Email:${elem.email}`;
+    let mobile = document.createElement("p");
+    mobile.innerText = `Mobile:${elem.mobile}`;
+    let city = document.createElement("p");
+    city.innerText = `City:${elem.city}`;
+    let pincode = document.createElement("p");
+    pincode.innerText = `Pincode:${elem.pincode}`;
 
-  })
-}
+    box.append(image1, firstname, dateofbirth, email, mobile, city, pincode);
+    all_products.append(box);
+  });
+};
 
 append(formdata);
