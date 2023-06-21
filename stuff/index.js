cdceb91a6b1fd4591951adb4a8e0994e2d9a5110
Nandlal <shahnandlal49@gmail.com>
2023-06-22 01:10:56 +0530

    all basic functionalities and cards are added

--- stuff/index.js
+++ stuff/index.js
@@ -5,8 +5,7 @@ function table(event) {
   event.preventDefault();
   window.location.reload();
   let form = document.getElementById("employeedata");
-
-  let firstname = form.firstname.value;
+let firstname = form.firstname.value;
   let lastname = form.lastname.value;
   let birthdate = form.birthdate.value;
   let email = form.email.value;
@@ -23,19 +22,13 @@ function table(event) {
     city,
     pincode
   );
-
-  // var validateError = {};
-  // const naamvalidation = /^[a-zA-Z]*$/;
-  // const televalidation = /^\d{10}$/;
-  // const emailvalidation = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
-
-  formdata.push(datafromconstruct);
+var validateError = {};
+  const naamvalidation = /^[a-zA-Z]*$/;
+  const televalidation = /^\d{10}$/;
+  const emailvalidation = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+ formdata.push(datafromconstruct);
   localStorage.setItem("data", JSON.stringify(formdata));
 }
-
-// Creating a constructor function;
-
 function construct(f, l, b, e, m, c, p) {
   this.firstname = f;
   this.lastname = l;
