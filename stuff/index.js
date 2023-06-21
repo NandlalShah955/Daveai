96a2512b7174b7df2e3bf6a27482e9429acee10c
Nandlal <shahnandlal49@gmail.com>
2023-06-21 23:54:03 +0530

    all things are added

--- stuff/index.js
+++ stuff/index.js
@@ -35,4 +35,33 @@ function construct(f,l,b,e,m,c,p){
 
 // Creating a function for appending data of the form in table;
 
- 
+ const append=(formdata)=>{
+
+  formdata.map(function(elem){
+  
+    var tr=document.createElement("tr");
+    
+    var td1=document.createElement("td");
+    td1.innerText=elem.firstname
+    var td2=document.createElement("td");
+    td2.innerText=elem.lastname
+    var td3=document.createElement("td");
+    td3.innerText=elem.birthdate
+    var td4=document.createElement("td");
+    td4.innerText=elem.email
+    var td5=document.createElement("td");
+    td5.innerText=elem.mobile
+    var td6=document.createElement("td");
+    td6.innerText=elem.city
+    var td7=document.createElement("td");
+    td7.innerText=elem.pincode
+
+    
+tr.append(td1,td2,td3,td4,td5,td6,td7);
+document.querySelector("tbody").append(tr);
+
+
+  })
+}
+
+append(formdata);
